4e5be4c8108431ac4dc7903261d97824cb05aea8
Patryk Borkowski <110451942+Pgph@users.noreply.github.com>
2022-11-10 11:59:16 +0100

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 //express app
@@ -15,7 +17,18 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening for requests on port", process.env.PORT);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Connected to db & listening for requests on port",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
