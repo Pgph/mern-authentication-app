f9330808e5bebfb074966d430a27cde4ec112193
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-11 15:33:54 +0100

    user controller & model, and set up some routes for authentication.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,6 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/workouts" element={<Workouts />} />
-            <Route path="/workouts/:id" element={<Workout />} />
-            <Route path="/workouts/:id/edit" element={<EditWorkout />} />
-            <Route path="/workouts/new" element={<NewWorkout />} /> */}
           </Routes>
         </div>
       </BrowserRouter>
