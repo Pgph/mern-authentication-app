73492cf006aade8cb9e4d49feac8d1edfd1e0b21
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-11 18:19:31 +0100

    sign tokens and send them back to the client.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, {
+    expiresIn: "3d",
+  });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,7 +18,11 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    //create a tokeb
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
