ff8d98e1b4db35c6a6576f612e0bf65f658bdabf
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-13 00:23:32 +0100

    Setting initial Auth Status

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,15 +16,14 @@ const userSchema = new Schema({
   },
 });
 
-//static signup method
+// static signup method
 userSchema.statics.signup = async function (email, password) {
-  //validation
+  // validation
   if (!email || !password) {
     throw Error("All fields must be filled");
   }
-
   if (!validator.isEmail(email)) {
-    throw Error("Invalid email");
+    throw Error("Email not valid");
   }
   if (!validator.isStrongPassword(password)) {
     throw Error("Password not strong enough");
@@ -33,37 +32,31 @@ userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
 
   if (exists) {
-    throw Error("Email already exists");
+    throw Error("Email already in use");
   }
 
-  //hash password
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  //create new user
   const user = await this.create({ email, password: hash });
 
   return user;
 };
 
-//static login method
+// static login method
 userSchema.statics.login = async function (email, password) {
   if (!email || !password) {
     throw Error("All fields must be filled");
   }
 
-  if (!validator.isEmail(email)) {
-    throw Error("Invalid email");
-  }
-
   const user = await this.findOne({ email });
   if (!user) {
-    throw Error("Invalid email");
+    throw Error("Incorrect email");
   }
 
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
-    throw Error("Invalid password");
+    throw Error("Incorrect password");
   }
 
   return user;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,38 +4,34 @@ import { useLogin } from "../hooks/useLogin";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { login, isLoading, error } = useLogin();
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await login({ email, password });
+    await login(email, password);
   };
 
   return (
-    <div>
-      <form className="login" onSubmit={handleSubmit}>
-        <h3>Login</h3>
-        <label>Email:</label>
-        <input
-          autoComplete="on"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password:</label>
-        <input
-          autoComplete="on"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button disabled={isLoading}>Login</button>
-        {error && <div className="error">{error}</div>}
-      </form>
-    </div>
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Log In</h3>
+
+      <label>Email address:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
+    </form>
   );
 };
 
