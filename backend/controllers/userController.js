1ef79454d6c17d679c4514dd63195454b6fee04e
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-12 02:59:42 +0100

    Added login user connection

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,19 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+
   res.json({ message: "login user" });
 };
 
@@ -19,7 +32,7 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    //create a tokeb
+    //create a token
     const token = createToken(user._id);
 
     res.status(200).json({ email, token });
